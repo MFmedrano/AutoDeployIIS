e8c35e9c48d05bad73a330b9ef2d2490388ee4fe
rmedrano <randalles007@yahoo.es>
2022-02-05 14:51:47 -0600

    update testDeploy

---
+++ TESTDEPLOY/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TESTDEPLOY.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
