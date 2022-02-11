b9acdef2f3bc76709500bb2faa855689f95f8d69
MFmedrano <randalles007@yahoo.es>
2022-02-11 12:29:27 -0600

    update jenkins file

--- TESTDEPLOY/Controllers/HomeController.cs
+++ TESTDEPLOY/Controllers/HomeController.cs
@@ -15,6 +15,7 @@ namespace TESTDEPLOY.Controllers
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+
         }
 
         public IActionResult Index()
